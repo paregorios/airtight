227c1f808bf58fa8b6cad3a921f3ee800d3129ac
Tom Elliott <tom.elliott@nyu.edu>
2017-10-08 18:39:58 -0400

    bump version to 0.2.0 for 'real' pypi release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name='airtight',
-    version='0.2.0a3',
+    version='0.2.0',
     description='Components for scripts in python 3.6+',
     long_description=long_description,
     url='https://github.com/paregorios/airtight',
